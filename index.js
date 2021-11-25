f21a93a7446f99b2f0efde7dc9c1849ebeb970ff
Maurice Bolhuis <maurice.bolhuis@compliance-innovations.com>
2021-11-25 13:13:16 +0100

    Add ecs run task custom-args option (RM-1368)

--- index.js
+++ index.js
@@ -89,6 +89,7 @@ async function run() {
     const cluster = core.getInput('cluster', { required: false });
     const count = core.getInput('count', { required: true });
     const startedBy = core.getInput('started-by', { required: false }) || agent;
+    const customArgs = core.getInput('custom-args', { required: false });
     const waitForFinish = core.getInput('wait-for-finish', { required: false }) || false;
     let waitForMinutes = parseInt(core.getInput('wait-for-minutes', { required: false })) || 30;
     if (waitForMinutes > MAX_WAIT_MINUTES) {
@@ -117,19 +118,20 @@ async function run() {
 
     const clusterName = cluster ? cluster : 'default';
 
-    core.debug(`Running task with ${JSON.stringify({
+    const runTaskArgs = {
       cluster: clusterName,
       taskDefinition: taskDefArn,
       count: count,
-      startedBy: startedBy
-    })}`)
+      startedBy: startedBy,
+    }
 
-    const runTaskResponse = await ecs.runTask({
-      cluster: clusterName,
-      taskDefinition: taskDefArn,
-      count: count,
-      startedBy: startedBy
-    }).promise();
+    if (customArgs.length > 0 ) {
+      Object.assign(runTaskArgs, JSON.parse(customArgs))
+    }
+
+    core.debug(`Running task with ${JSON.stringify(runTaskArgs)}`)
+
+    const runTaskResponse = await ecs.runTask(runTaskArgs).promise();
 
     core.debug(`Run task response ${JSON.stringify(runTaskResponse)}`)
 
@@ -161,7 +163,7 @@ async function waitForTasksStopped(ecs, clusterName, taskArns, waitForMinutes) {
   const maxAttempts = (waitForMinutes * 60) / WAIT_DEFAULT_DELAY_SEC;
 
   core.debug('Waiting for tasks to stop');
-  
+
   const waitTaskResponse = await ecs.waitFor('tasksStopped', {
     cluster: clusterName,
     tasks: taskArns,
@@ -172,7 +174,7 @@ async function waitForTasksStopped(ecs, clusterName, taskArns, waitForMinutes) {
   }).promise();
 
   core.debug(`Run task response ${JSON.stringify(waitTaskResponse)}`)
-  
+
   core.info(`All tasks have stopped. Watch progress in the Amazon ECS console: https://console.aws.amazon.com/ecs/home?region=${aws.config.region}#/clusters/${clusterName}/tasks`);
 }
 
@@ -187,7 +189,7 @@ async function tasksExitCode(ecs, clusterName, taskArns) {
   const reasons = containers.map(container => container.reason)
 
   const failuresIdx = [];
-  
+
   exitCodes.filter((exitCode, index) => {
     if (exitCode !== 0) {
       failuresIdx.push(index)
--- index.test.js
+++ index.test.js
@@ -34,7 +34,8 @@ describe('Deploy to ECS', () => {
             .mockReturnValueOnce('task-definition.json')                      // task-definition
             .mockReturnValueOnce('cluster-789')                               // cluster
             .mockReturnValueOnce('1')                                         // count
-            .mockReturnValueOnce('amazon-ecs-run-task-for-github-actions');   // started-by
+            .mockReturnValueOnce('amazon-ecs-run-task-for-github-actions')    // started-by
+            .mockReturnValueOnce('{"launchType":"FARGATE"}')                  // custom-args
 
         process.env = Object.assign(process.env, { GITHUB_WORKSPACE: __dirname });
 
@@ -130,9 +131,36 @@ describe('Deploy to ECS', () => {
             cluster: 'cluster-789',
             taskDefinition: 'task:def:arn',
             count: '1',
-            startedBy: 'amazon-ecs-run-task-for-github-actions'
+            startedBy: 'amazon-ecs-run-task-for-github-actions',
+            launchType: "FARGATE"
         });
         expect(mockEcsWaiter).toHaveBeenCalledTimes(0);
+        expect(core.setOutput).toBeCalledWith('task-arn', ['arn:aws:ecs:fake-region:account_id:task/arn']);
+    });
+
+    test('does not include custom args when none are given', async () => {
+        core.getInput = jest
+            .fn()
+            .mockReturnValueOnce('task-definition.json')                      // task-definition
+            .mockReturnValueOnce('cluster-789')                               // cluster
+            .mockReturnValueOnce('1')                                         // count
+            .mockReturnValueOnce('amazon-ecs-run-task-for-github-actions')    // started-by
+            .mockReturnValueOnce('')                                          // custom-args
+            .mockReturnValueOnce('true');                                     // wait-for-finish
+
+        await run();
+        expect(core.setFailed).toHaveBeenCalledTimes(0);
+
+        expect(mockEcsRegisterTaskDef).toHaveBeenNthCalledWith(1, { family: 'task-def-family'});
+        expect(core.setOutput).toHaveBeenNthCalledWith(1, 'task-definition-arn', 'task:def:arn');
+        expect(mockRunTasks).toHaveBeenNthCalledWith(1, {
+            cluster: 'cluster-789',
+            taskDefinition: 'task:def:arn',
+            count: '1',
+            startedBy: 'amazon-ecs-run-task-for-github-actions'
+
+        });
+        expect(mockEcsWaiter).toHaveBeenCalledTimes(1);
         expect(core.setOutput).toBeCalledWith('task-arn', ['arn:aws:ecs:fake-region:account_id:task/arn']);
     });
 
@@ -143,6 +171,7 @@ describe('Deploy to ECS', () => {
             .mockReturnValueOnce('cluster-789')                               // cluster
             .mockReturnValueOnce('1')                                         // count
             .mockReturnValueOnce('amazon-ecs-run-task-for-github-actions')    // started-by
+            .mockReturnValueOnce('{"launchType":"FARGATE"}')                  // custom-args
             .mockReturnValueOnce('true');                                     // wait-for-finish
 
         await run();
